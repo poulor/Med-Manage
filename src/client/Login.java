fc78b399d966ca0aeebc315d9352f794cfad1186
MicheleCabral <57777929+MicheleCabral@users.noreply.github.com>
2019-11-19 20:23:11 -0500

    Added Login Page

--- src/client/Login.java
+++ src/client/Login.java
@@ -1,40 +1,57 @@
 package client;
 
 import javafx.application.Application;
+import javafx.geometry.HPos;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
-
-public class Login extends Application {
-	@Override // Override the start method in the Application class
-	public void start(Stage primaryStage) {
-		Circle circle = new Circle();
-		Pane pane = new Pane();
-		circle.centerXProperty().bind(pane.widthProperty().divide(2));
-		circle.centerYProperty().bind(pane.widthProperty().divide(2));
-		
-		
-		circle.setStroke(Color.YELLOW);
-		circle.setFill(Color.FIREBRICK);
-		circle.setRadius(150.0f);
-		
-		
-		pane.getChildren().add(circle);
-		
-		Scene scene = new Scene(pane,500,500);
-		primaryStage.setTitle("CIRCLE");
+public class Login extends Application 
+{
+	private TextField un = new TextField();
+	private TextField pw = new TextField();
+	private Button signIn = new Button("Sign In");
+	
+	@Override
+	
+	public void start (Stage primaryStage)
+	{
+		GridPane pane = new GridPane();
+		pane.setHgap(5);
+		pane.setVgap(5);
+		pane.setAlignment(Pos.CENTER);
+		pane.add(new Label("Username: "), 0, 0);
+		pane.add(un, 1, 0);
+		pane.add(new Label("Password: "), 0, 1);
+		pane.add(pw, 1, 1);
+		pane.add(signIn, 1, 5);
+	    GridPane.setHalignment(signIn, HPos.RIGHT);
+	    
+	    BackgroundImage myBI= new BackgroundImage(new Image("background.jpg",600,400,false,true),
+	            BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER,
+	              new BackgroundSize(1.0, 1.0, true, true, false, false));
+	    pane.setBackground(new Background(myBI));
+	    
+	    Scene scene = new Scene(pane, 600, 400);
+		primaryStage.setTitle("Sign In");
 		primaryStage.setScene(scene);
 		primaryStage.show();
+		
 	}
-
-	public static void main(String[] args) {
-
+	
+	public static void main(String[] args) 
+	{
 		launch(args);
-		System.out.println("Hello World!");
-
-	}
-
+	}	
+			
 }
