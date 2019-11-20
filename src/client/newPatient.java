00e1e8a0f7b0699de7fe65984cab524b23c36ec6
MicheleCabral <57777929+MicheleCabral@users.noreply.github.com>
2019-11-19 20:30:21 -0500

    Created New Patient class

---
+++ src/client/newPatient.java
@@ -0,0 +1,22 @@
+package client;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class newPatient extends Application
+{
+	
+	
+	@Override
+	
+	public void start(Stage primarySage)
+	{
+		
+	}
+	
+	public static void main(String[] args) 
+	{
+		launch(args);
+	}
+}
+
