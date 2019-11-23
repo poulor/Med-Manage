e3929ed2e74362021753e4f9bcb25b3ba2b28388
MicheleCabral <57777929+MicheleCabral@users.noreply.github.com>
2019-11-23 13:53:48 -0500

    Added User Type

--- src/client/Login.java
+++ src/client/Login.java
@@ -1,10 +1,13 @@
 package client;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.HPos;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
@@ -20,6 +23,14 @@ public class Login extends Application
 {
 	private TextField un = new TextField();
 	private TextField pw = new TextField();
+	ObservableList<String> userType = 
+			FXCollections.observableArrayList(
+					"Patient",
+					"Doctor",
+					"Receptionist",
+					"Administrator"
+					);
+	private ComboBox usertype = new ComboBox(userType);
 	private Button signIn = new Button("Sign In");
 	
 	@Override
@@ -34,13 +45,17 @@ public class Login extends Application
 		pane.add(un, 1, 0);
 		pane.add(new Label("Password: "), 0, 1);
 		pane.add(pw, 1, 1);
+		pane.add(new Label("User Type: "), 0, 2);
+		pane.add(usertype, 1, 2);
 		pane.add(signIn, 1, 5);
 	    GridPane.setHalignment(signIn, HPos.RIGHT);
 	    
-	    BackgroundImage myBI= new BackgroundImage(new Image("background.jpg",600,400,false,true),
-	            BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER,
-	              new BackgroundSize(1.0, 1.0, true, true, false, false));
-	    pane.setBackground(new Background(myBI));
+	   /* 
+	    * BackgroundImage myBI= new BackgroundImage(new Image("https://cdn1.imggmi.com/uploads/2019/11/23/a5dfc21978b80f30e973c097f47a6f26-full.jpg",600,400,false,true),
+	    *        BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER,
+	    *         new BackgroundSize(1.0, 1.0, true, true, false, false));
+	    * pane.setBackground(new Background(myBI));
+	    */
 	    
 	    Scene scene = new Scene(pane, 600, 400);
 		primaryStage.setTitle("Sign In");
