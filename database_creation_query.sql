7cb7aab15d1a5dd8ef5931241c1d9f3fecc15658
Richard Poulose <rjpoulose@gmail.com>
2019-11-23 17:01:06 -0500

    Updated database table names, contents and relationships. Also added Administrators table

--- database_creation_query.sql
+++ database_creation_query.sql
@@ -1,15 +1,20 @@
--- drop database if exists javabook;
+drop database if exists MedicalClient;
 create database MedicalClient;
 -- drop user if exists 'scott'@'localhost';
 -- create user 'scott'@'localhost' identified by 'tiger';
 -- grant select, insert, update, delete, create, create view, drop, execute, references on javabook.* to 'scott'@'localhost';
 
 use MedicalClient;
--- drop table if exists Student;
--- drop table if exists Course;
--- drop table if exists Enrollment;
+drop table if exists Users;
+drop table if exists Administrators;
+drop table if exists Appointments;
+drop table if exists Doctors;
+drop table if exists Patients;
+drop table if exists Receptionists;
+
 
 create table Users ( 
+  user_id INT NOT NULL AUTO_INCREMENT,
   firstName varchar(25), 
   midName char(1), 
   lastName varchar(25), 
@@ -18,8 +23,9 @@ create table Users (
   street varchar(25), 
   zipCode char(5),
   deptId char(4),  
-  primary key (ssn)
+  primary key (user_id)
 ); 
+
 
 create table Administrators (
   courseId char(5),
@@ -74,94 +80,14 @@ insert into Student values (
 insert into Student values (
   '444111111', 'John', 'K', 'Stevenson', '9129219434',
   null, '100 Main Street', '31411', 'BIOL');
-insert into Student values (
-  '444111112', 'George', 'R', 'Heintz', '9129213454',
-  '1974-10-10', '1200 Abercorn Street', '31419', 'CS');
-insert into Student values (
-  '444111113', 'Frank', 'E', 'Jones', '9125919434',
-  '1970-09-09', '100 Main Street', '31411', 'BIOL');
-insert into Student values (
-  '444111114', 'Jean', 'K', 'Smith', '9129219434',
-  '1970-02-09', '100 Main Street', '31411', 'CHEM');
-insert into Student values (
-  '444111115', 'Josh', 'R', 'Woo', '7075989434',
-  '1970-02-09', '555 Franklin Street', '31411', 'CHEM');
-insert into Student values (
-  '444111116', 'Josh', 'R', 'Smith', '9129219434',
-  '1973-02-09', '100 Main Street', '31411', 'BIOL');
-insert into Student values (
-  '444111117', 'Joy', 'P', 'Kennedy', '9129229434',
-  '1974-03-19', '103 Bay Street', '31412', 'CS');
-insert into Student values (
-  '444111118', 'Toni', 'R', 'Peterson', '9129229434',
-  '1964-04-29', '103 Bay Street', '31412', 'MATH');
-insert into Student values (
-  '444111119', 'Patrick', 'R', 'Stoneman', null,
-  '1969-04-29', '101 Washington Street', '31435', 'MATH');
-insert into Student values (
-  '444111120', 'Rick', 'R', 'Carter', null,
-  '1986-04-09', '19 West Ford Street', '31411', 'BIOL');
-
-
 
 insert into Course values (
   '11111', 'CSCI', '1301', 'Intro to Java I', 4);
 insert into Course values (
   '11112', 'CSCI', '1302', 'Intro to Java II', 3);
-insert into Course values (
-  '11113', 'CSCI', '4720', 'Database Systems', 3);
-insert into Course values (
-  '11114', 'CSCI', '4750', 'Rapid Java Application', 3);
-insert into Course values (
-  '11115', 'MATH', '2750', 'Calculus I', 5);
-insert into Course values (
-  '11116', 'MATH', '3750', 'Calculus II', 5);
-insert into Course values (
-  '11117', 'EDUC', '1111', 'Reading', 3);
-insert into Course values (
-  '11118', 'ITEC', '1344', 'Database Administration', 3);
 
 insert into Enrollment values (
   '444111110', '11111', now(), 'A');
 /* In MS Access, replace now() by date() */
 insert into Enrollment values (
   '444111110', '11112', now(), 'B');
-insert into Enrollment values (
-  '444111110', '11113', now(), 'C');
-insert into Enrollment values (
-  '444111111', '11111', now(), 'D');
-insert into Enrollment values (
-  '444111111', '11112', now(), 'F');
-insert into Enrollment values (
-  '444111111', '11113', now(), 'A');
-insert into Enrollment values (
-  '444111112', '11111', now(), null);
-insert into Enrollment values (
-  '444111112', '11112', now(), null);
-insert into Enrollment values (
-  '444111112', '11114', now(), 'B');
-insert into Enrollment values (
-  '444111112', '11115', now(), 'C');
-insert into Enrollment values (
-  '444111112', '11116', now(), null);
-insert into Enrollment values (
-  '444111113', '11111', now(), null);
-insert into Enrollment values (
-  '444111113', '11113', now(), null);
-insert into Enrollment values (
-  '444111114', '11115', now(), null);
-insert into Enrollment values (
-  '444111115', '11115', now(), null);
-insert into Enrollment values (
-  '444111115', '11116', now(), null);
-insert into Enrollment values (
-  '444111116', '11111', now(), null);
-insert into Enrollment values (
-  '444111117', '11111', now(), null);
-insert into Enrollment values (
-  '444111118', '11111', now(), null);
-insert into Enrollment values (
-  '444111118', '11112', now(), null);
-insert into Enrollment values (
-  '444111118', '11113', now(), null);
-
