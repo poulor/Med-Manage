6fc461aebd594c709b3272e790c0ef7a5425e894
poulor <rjpoulose@gmail.com>
2019-12-03 16:20:22 -0500

    Added doctor_id to constructor

--- src/client/Doctor.java
+++ src/client/Doctor.java
@@ -6,11 +6,13 @@ import java.time.format.DateTimeFormatter;
 
 public class Doctor{
     static int number_of_doctors = 0;
+    private int doctor_id;
     private LocalDate join_date;
     private String specialty;
     private String department;
    
     // ==[Constructor]==
+    // Need to add user_id
     public Doctor(String join_date, String specialty, String department) {
 
         // Formatting join_date string to LocalDate format
@@ -19,8 +21,8 @@ public class Doctor{
         this.join_date = LocalDate.parse(join_date, formatter);
         this.specialty = specialty;
         this.department = department;
-        Doctor.number_of_users += 1;
-        this.user_id = Doctor.number_of_users;
+        Doctor.number_of_doctors += 1;
+        this.doctor_id = Doctor.number_of_doctors;
 
     }
 
