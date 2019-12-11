1e343a326314ed8753ff995eb9518b7a78df0f6f
MicheleCabral <57777929+MicheleCabral@users.noreply.github.com>
2019-12-10 21:29:58 -0500

    working on db

--- src/client/AppointmentsOfDay.java
+++ src/client/AppointmentsOfDay.java
@@ -1,23 +1,49 @@
 package client;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class AppointmentsOfDay extends Application
 {
-	
+	private PreparedStatement preparedStatement;
+	private TextField date = new TextField();
+	private Label lblStatus = new Label();
 	@Override
 	
 	public void start(Stage primaryStage)
 	{
+		
+		initializeDB();
+		
+		Button ShowAppointments = new Button("Show Appointments");
+	    HBox hBox = new HBox(5);
+	    hBox.getChildren().addAll(new Label("Date (MM/DD/YYYY): "), date, 
+	      (ShowAppointments));
 
+	    VBox vBox = new VBox(10);
+	    vBox.getChildren().addAll(hBox, lblStatus);
+	     
+	    date.setPrefColumnCount(6);
+	    ShowAppointments.setOnAction(e -> showAppointments());
+/*
         TableView<Appointment> appTable = new TableView<>();
         TableColumn<Appointment, String> firstNameCol = new TableColumn<>("First Name");
         firstNameCol.setCellValueFactory(new PropertyValueFactory<>("firstName"));
@@ -29,13 +55,50 @@ public class AppointmentsOfDay extends Application
         appTable.getColumns().addAll(firstNameCol, lastNameCol, timeCol);
 
       //  appTable.getItems().addAll(dataAccessor.getPersonList());
-
-        BorderPane root = new BorderPane();
-        root.setCenter(appTable);
-        Scene scene = new Scene(root, 600, 400);
+*/
+        //BorderPane root = new BorderPane();
+       // root.setCenter(appTable);
+        Scene scene = new Scene(vBox, 600, 400);
+        primaryStage.setTitle("Appointments");
         primaryStage.setScene(scene);
         primaryStage.show();
 	}
+	
+	private void initializeDB() {    
+		  try {
+		      Class.forName("com.mysql.jdbc.Driver");      // Load the JDBC driver
+		       // Establish a connection
+		      Connection connection = DriverManager.getConnection
+		        ("jdbc:mysql://localhost/MedicalClient", "test_user", "password");
+		      System.out.println("Database connected");
+		      String queryString = "select Users.first_name, " +
+		        "Users.last_name, from Appointments, Users " +
+		        "where Appointments.appointment_date = ? ";
+		      preparedStatement = connection.prepareStatement(queryString);    
+			  }
+		    catch (Exception ex) {  ex.printStackTrace();  }  }
+	
+	private void showAppointments() {
+	    String dt = date.getText();
+	    try { preparedStatement.setString(1, dt);
+	       ResultSet rset = preparedStatement.executeQuery();
+
+	      if (rset.next()) {
+	        String lastName = rset.getString(1);
+	        String firstName = rset.getString(3);
+	        //String appointment_time = rset.getString(3);
+
+
+	        // Display result in a label
+	        lblStatus.setText(firstName + " " + lastName + "\n");// +  appointment_time + "\n");
+	      } else {
+	        lblStatus.setText("Not found");
+	      }
+	    }
+	    catch (SQLException ex) {
+	      ex.printStackTrace();
+	    }
+	  }
 	
 	public static void main(String[] args) 
 	{
