cbfa49759c68f319323b72cc5f85714570bfdc10
poulor <rjpoulose@gmail.com>
2019-12-12 19:55:37 -0500

    Removed Appointments.java

--- database_insertion_query.sql
+++ database_insertion_query.sql
@@ -1,8 +1,13 @@
--- INSERT INTO Users VALUES ( NULL, "Bill", "", "Nye", "Male", "1955-04-24", "(780) 242-8071", "74 N. Henry Drive", "Waukegan", "IL", 60085, "bnye@gmail.com", "bnye", "password", "DOCTOR");
--- INSERT INTO Doctors VALUES(NULL, "1985-05-12", "","", 1);
--- 
--- INSERT INTO Users VALUES ( NULL, "Will", "", "Smith", "Male", "1968-09-25", "(975) 400-9824", "349 Fairview Ave.", "Goose Creek", "SC", 29445, "wsmith@aol.com", "wwwsmith", "password", "Patient");
--- INSERT INTO Patients VALUES (NULL, 174, 190, "", "", "", 2, 1);
+INSERT INTO Users VALUES ( NULL, "Bill", "", "Nye", "Male", "1955-04-24", "(780) 242-8071", "74 N. Henry Drive", "Waukegan", "IL", 60085, "bnye@gmail.com", "bnye", "password", "DOCTOR");
+INSERT INTO Doctors VALUES(NULL, "1985-05-12", "","", 1);
+
+INSERT INTO Users VALUES ( NULL, "Santa", "", "Claus", "Male", "1968-09-25", "(975) 400-9824", "349 Fairview Ave.", "Goose Creek", "SC", 29445, "wsmith@aol.com", "wwwsmith", "password", "Patient");
+INSERT INTO Patients VALUES (NULL, 174, 190, "", "", "", 2, 1);
+
+INSERT INTO Users VALUES ( NULL, "Will", "", "Smith", "Male", "1990-12-25", "(975) 400-9824", "349 Fairview Ave.", "Goose Creek", "SC", 29445, "wsmith@aol.com", "wwwsmith", "password", "Patient");
+INSERT INTO Patients VALUES (NULL, 174, 190, "", "", "", 3, 1);
 
 
 INSERT INTO Appointments VALUES (NULL, "2019-10-10", "08:30:00", "", 1, 2);
+-- INSERT INTO Appointments VALUES (NULL, "2019-12-10", "10:30:00", "", 1, 2);
+-- INSERT INTO Appointments VALUES (NULL, "2019-10-10", "11:30:00", "", 1, 2);
